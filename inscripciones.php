8be7b8d3c168e28d936a82e2f202d60e3d2e464a
Sergio Cañete Linares <sergio13896@gmail.com>
2024-02-04 16:52:47 +0100

    mas

--- inscripciones.php
+++ inscripciones.php
@@ -32,9 +32,6 @@ $candidaturas = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Enlace a tus estilos CSS -->
-    <link rel="stylesheet" href="styles.css">
-
     <!-- Enlace a Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
